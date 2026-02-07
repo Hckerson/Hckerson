2b400975b4bfb53f6959dccbfb7820eb45a3e6f0
hckerson <hckerson@gmail.com>
2026-02-07 18:26:35 +0100

    feat: Implement initial styling setup with TailwindCSS, add Prettier configuration, and create the HeroSection component.

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,10 +1,27 @@
-
 export default function HeroSection() {
-    return(
-        <section className="w-full box-border h-screen">
-            <div>
-
+    return (
+        <section className="relative box-border h-screen w-full">
+            <div className="absolute inset-0">
+                <div className="animate-slide-infinitely absolute inset-0 top-0 left-0 bg-transparent">
+                    <div className="h-screen w-1/3 -translate-y-[250px] -rotate-45">
+                        <div className="flex h-1/2 w-full">
+                            <div className="h-full w-1/2"></div>
+                            <div className="h-full w-1/2"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="animate-slide-infinitely absolute inset-0 top-0 right-0 bg-transparent delay-5000!">
+                    <div className="relative h-full w-full">
+                        <div className="h-screen w-1/3 -translate-y-[250px] rotate-45 absolute top-0 right-0">
+                            <div className="flex h-1/2 w-full">
+                                <div className="h-full w-1/2"></div>
+                                <div className="h-full w-1/2"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div></div>
         </section>
-    )
+    );
 }
