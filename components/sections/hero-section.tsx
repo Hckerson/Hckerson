8a8c67e3c072e6c76eb4831583632a95b26aa140
hckerson <hckerson@gmail.com>
2026-02-09 00:41:51 +0100

    feat: Add HeroSection component and accompanying global styles with animations and utility classes.

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,21 +1,21 @@
 export default function HeroSection() {
     return (
         <section className="relative box-border h-screen w-full">
-            <div className="absolute inset-0">
-                <div className="animate-slide-infinitely absolute inset-0 top-0 left-0 bg-transparent">
-                    <div className="h-screen w-1/3 -translate-y-[250px] -rotate-45">
-                        <div className="flex h-1/2 w-full">
-                            <div className="h-full w-1/2"></div>
-                            <div className="h-full w-1/2"></div>
+            <div className="absolute inset-0 overflow-hidden">
+                <div className="animate-slide-left-to-right absolute inset-0 top-0 left-0 bg-transparent ">
+                    <div className="flex h-screen w-fit -translate-y-[450px] -rotate-45">
+                        <div className="flex h-full -space-x-10">
+                            <div className="blury h-full w-[250px]"></div>
+                            <div className="blury-01 h-full w-[250px]"></div>
                         </div>
                     </div>
                 </div>
-                <div className="animate-slide-infinitely absolute inset-0 top-0 right-0 bg-transparent delay-5000!">
+                <div className="animate-slide-right-to-left absolute inset-0 top-0 right-0 bg-transparent ">
                     <div className="relative h-full w-full">
-                        <div className="h-screen w-1/3 -translate-y-[250px] rotate-45 absolute top-0 right-0">
-                            <div className="flex h-1/2 w-full">
-                                <div className="h-full w-1/2"></div>
-                                <div className="h-full w-1/2"></div>
+                        <div className="absolute top-0 right-0 flex h-screen w-fit -translate-y-[450px] rotate-45">
+                            <div className="flex h-full -space-x-10	">
+                                <div className="blury-01 h-full w-[250px]"></div>
+                                <div className="blury h-full w-[250px]"></div>
                             </div>
                         </div>
                     </div>
