a528b5a43e41711d4f8626486f231a4a09434fc0
hckerson <hckerson@gmail.com>
2026-02-15 00:00:52 +0100

    feat: Introduce AboutSection and a dynamic Navbar, and integrate Prettier into ESLint.

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,20 +1,37 @@
+"use client";
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 
-const createConnection = (serverUrl: string, roomId: string) => {
-    return {
-        disconnect: () => {},
-        connect: () => {},
-    };
-};
-
-function ChatRoom() {
-    const [roomId, setRoomId] = useState("1");
-    const [serverUrl, setServerUrl] = useState("https://localhost:1234");
-
+export default function Navbar() {
+    const [animationStarted, setAnimationStarted] = useState<Boolean>(false);
     useEffect(() => {
-        const connection = createConnection(serverUrl, roomId);
-        connection.connect();
-        return () => connection.disconnect();
-    }, []); // 🔴 React Hook useEffect has missing dependencies: 'roomId' and 'serverUrl'
-    // ...
+        const scrollController = (e: Event) => {
+            const verticalOffset = window.scrollY;
+            if (verticalOffset > 100) {
+                setAnimationStarted(true);
+            }
+        };
+
+        window.addEventListener("scroll", scrollController);
+
+        return () => {
+            window.removeEventListener("scroll", () => {
+                setAnimationStarted(false);
+            });
+        };
+    }, []);
+    return (
+        <nav
+            className={clsx("fixed flex h-25 w-full items-end bg-transparent")}
+        >
+            <div
+                className={clsx(
+                    "mx-auto h-fit transition-transform duration-300 ease-in-out",
+                    animationStarted ? "-translate-y-10" : "bottom-0",
+                )}
+            >
+                sup
+            </div>
+        </nav>
+    );
 }
