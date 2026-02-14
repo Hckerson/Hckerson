a2d1b77e9a67b335c127e1df3e2444c1a0b19374
hckerson <hckerson@gmail.com>
2026-02-15 00:08:52 +0100

    feat: Add initial About section layout with Tailwind CSS and configure Prettier.

--- components/sections/about-section.tsx
+++ components/sections/about-section.tsx
@@ -1,10 +1,15 @@
 
+
+const fallback = "HK"
 export default function AboutSection() {
-  return(
-    <div>
-      <div>
-        
-      </div>
-    </div>
-  )
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <div className="flex gap-x-30">
+                <div className="flex flex-col">
+
+				</div>
+                <div></div>
+            </div>
+        </div>
+    );
 }
