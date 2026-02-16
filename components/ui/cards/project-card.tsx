af8fbd047768c0d71e46fe0636df7dfed9475203
hckerson <hckerson@gmail.com>
2026-02-16 23:48:24 +0100

    feat: add placeholder content and navigation links to ProjectCard.

--- components/ui/cards/project-card.tsx
+++ components/ui/cards/project-card.tsx
@@ -1,13 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProjectCard() {
     return (
         <div className="flex overflow-hidden rounded-xl md:rounded-2xl xl:rounded-3xl">
-            <div className="flex-1 h-100 flex items-center justify-center">
-
+            <div className="flex h-100 flex-1 items-center justify-center">
+                <div className="space-y-4">
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                    <div>
+                        <ul className="flex w-full gap-x-2">
+                            <li></li>
+                        </ul>
+                    </div>
+                    <Link href={"/projects"}>
+                        <p></p>
+                    </Link>
+                    <Link href={"/projects"}>
+                        <p></p>
+                    </Link>
+                </div>
             </div>
             <div className="h-full w-full flex-1">
-                <Image src={""} alt="" className="aspect-9/16"/>
+                <Image src={""} alt="" className="aspect-9/16" />
             </div>
         </div>
     );
