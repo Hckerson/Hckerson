6f027f35a3b72fba7c2cad3155b94cfd330d07f0
hckerson <hckerson@gmail.com>
2026-02-16 23:44:50 +0100

    feat: integrate Next.js Image component and establish flexbox layout for ProjectCard.

--- components/ui/cards/project-card.tsx
+++ components/ui/cards/project-card.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 
-export default function ProjectCard(){
-    return(
-        <div className="">
-            <div>
+export default function ProjectCard() {
+    return (
+        <div className="flex overflow-hidden rounded-xl md:rounded-2xl xl:rounded-3xl">
+            <div className="flex-1 h-100 flex items-center justify-center">
 
             </div>
+            <div className="h-full w-full flex-1">
+                <Image src={""} alt="" className="aspect-9/16"/>
+            </div>
         </div>
-    )
+    );
 }
